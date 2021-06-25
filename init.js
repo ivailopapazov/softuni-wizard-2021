8b51316ff72458aeec59519e64ec14a1d7954299
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-06-25 19:05:36 +0300

    Add wizard on startgame

--- init.js
+++ init.js
@@ -3,6 +3,9 @@ function gameFactory() {
 
     let startScreen = document.querySelector('.start-screen');
     let playScreen = document.querySelector('.play-screen');
+    let wizardElement = createWizard(100, 200);
+
+    playScreen.appendChild(wizardElement);
 
     Object.defineProperties(factory, {
         startScreen: {
@@ -10,8 +13,18 @@ function gameFactory() {
         },
         playScreen: {
             get: () => playScreen
-        }
+        },
+        wizardElement
     });
 
     return factory;
+}
+
+function createWizard(posX, posY) {
+    let wizardElement = document.createElement('div');
+    wizardElement.classList.add('wizard');
+    wizardElement.style.top = posY + 'px';
+    wizardElement.style.left = posX + 'px';
+
+    return wizardElement;
 }
