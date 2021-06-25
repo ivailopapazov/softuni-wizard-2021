0fa8d5e5861261f6475491f7b080e17c528eae9f
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-06-25 21:38:17 +0300

    Create fireball in current player position

--- gameState.js
+++ gameState.js
@@ -1,26 +1,24 @@
-function gameStateFactory() {
-    let state = {
-        gameOver: false,
-        score: 0,
-        wizard: {
-            x: 100,
-            y: 200,
-            width: 82,
-            height: 100,
-            speed: 7,
-            gravity: 2,
-        },
-        keys: {},
-        bugStats: {
-            nextBugCreation: 0,
-            maxCreationInterval: 1500,
-            speed: 3,
-            width: 50,
-            height: 50,
-        }
-    };
-    
-    return function() {
-        return state;
-    };
-}
+var state = {
+    gameOver: false,
+    score: 0,
+    wizard: {
+        x: 100,
+        y: 200,
+        width: 82,
+        height: 100,
+        speed: 7,
+        gravity: 2,
+    },
+    keys: {},
+    bugStats: {
+        nextBugCreation: 0,
+        maxCreationInterval: 1500,
+        speed: 3,
+        width: 50,
+        height: 50,
+    },
+    fireballStats: {
+        width: 40,
+        height: 40,
+    }
+};
